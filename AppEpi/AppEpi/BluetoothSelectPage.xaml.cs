5cf18b28848599c541a1956d034270d4685f2e54
Rodolfo Cortese <rodolfo.cortese@easyepi.com.br>
2019-03-22 18:31:29 -0300

    Seleção de dispositivo pareado (básico)
    Integração da tela de seleção de dispositivo com o módulo de Bluetooth.
    
    Known issues:
    - Quando é solicitada conexão a dois dispositivos diferentes temos uma série de erros
    - O layout ainda não combina completamente

--- AppEpi/AppEpi/BluetoothSelectPage.xaml.cs
+++ AppEpi/AppEpi/BluetoothSelectPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,20 +8,36 @@ namespace AppEpi
 {
     public partial class BluetoothSelectPage : ContentPage
     {
+        private ObservableCollection<string> _listOfDevices = new ObservableCollection<string>();
+
+        private string _selectedDeviceName = null;
+
         public BluetoothSelectPage()
         {
             InitializeComponent();
+
+            try
+            {
+                _listOfDevices = DependencyService.Get<IBluetoothController>().GetPairedDevices();
+                deviceList.ItemsSource = _listOfDevices;
+
+                deviceList.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
+                {
+                    _selectedDeviceName = deviceList.SelectedItem.ToString();
+                    btnConectar.IsVisible = true;
+                };
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception no povoamento de dispositivos Bluetooth: " + e.Message);    
+            }
         }
 
 
-        async private void btnParear_Clicked(object sender, EventArgs e)
+        async private void btnConectar_Clicked(object sender, EventArgs e)
         {
-        }
-
-
-        async protected override void OnAppearing()
-        {
-            base.OnAppearing();
+            DependencyService.Get<IBluetoothController>().Start(_selectedDeviceName);
         }
     }
 }
