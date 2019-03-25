f1c569e18f35edbfc6e46d8cb9b55a9d99224dff
Rodolfo Cortese <rodolfo.cortese@easyepi.com.br>
2019-03-25 13:49:53 -0300

    EpcListView: módulo para lidar com lista de EPCs
    Já implementado em Consulta de EPI

--- AppEpi/AppEpi/CnEPI.xaml.cs
+++ AppEpi/AppEpi/CnEPI.xaml.cs
@@ -1,54 +1,28 @@
 ﻿using System;
 using Xamarin.Forms;
-using System.Diagnostics;
 using System.Collections.ObjectModel;
 
 namespace AppEpi
 {
     public partial class CnEPI : ContentPage
     {
-        private ObservableCollection<string> _epcList = new ObservableCollection<string>();
 
         public CnEPI()
         {
             InitializeComponent();
-
-            epcList.ItemsSource = _epcList;
-
-            MessagingCenter.Subscribe<App, string>(this, "EPC", (sender, arg) =>
-            {
-                _epcList.Add(arg);
-            });
         }
 
 
         async private void btnConfirmar_Clicked(object sender, EventArgs e)
         {
             var wbs = DependencyService.Get<IWEBClient>();
-            string WbsFormattedEpcList = "";
-            int count = 0;
 
-            
-
-            //string[] lines = epis.Text.Split('\n');
-            string[] epcList = new string[_epcList.Count];
-            _epcList.CopyTo(epcList, 0);
-
-            foreach (string epc in epcList)
+            if (epcList.Count > 0)
             {
-                if (epc != "")
-                {
-                    count++;
-                    WbsFormattedEpcList += "|" + epc;
-                }
-            }
-
-            if (count > 0)
-            {
-                var answer = await DisplayAlert("Consulta de Epi", "Confirmar Consulta?\nTotal de Itens:" + count, "Sim", "Não");
+                var answer = await DisplayAlert("Consulta de Epi", "Confirmar Consulta?\nTotal de Itens:" + epcList.Count, "Sim", "Não");
                 if (answer)
                 {
-                    var result = wbs.consultEPI(WbsFormattedEpcList);
+                    var result = wbs.consultEPI(epcList.GetFormattedEpcList());
                     UsuarioLogado.Operacao = "7";
                     var detailPage = new ResultadoTrn(result);
 
@@ -61,12 +35,11 @@ namespace AppEpi
                 await DisplayAlert("Consulta de Epi", "Verifique os Campos!", "OK");
             }
         }
-        
 
-        async protected override void OnAppearing()
+        protected override void OnAppearing()
         {
             base.OnAppearing();
-            _epcList.Clear();
+            epcList.Clear();
         }
     }
 }
---
+++ AppEpi/AppEpi/EpcListView.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace AppEpi
+{
+    public class EpcListView : ListView
+    {
+        public int Count { get => _epcList.Count; }
+
+        private ObservableCollection<string> _epcList = new ObservableCollection<string>();
+
+        public EpcListView()
+        {
+            ItemsSource = _epcList;
+
+            MessagingCenter.Subscribe<App, string>(this, "EPC", (sender, arg) =>
+            {
+                _epcList.Add(arg);
+            });
+        }
+
+
+        // remove todos os epcs da lista
+        public void Clear()
+        {
+            _epcList.Clear();
+        }
+
+
+        // retorna uma lista dos EPCs sendo exibido do modo como exige o WebService
+        // retorna null se não houverem itens
+        public string GetFormattedEpcList()
+        {
+            if (_epcList.Count > 0)
+            {
+                string WbsFormattedEpcList = "";
+
+                string[] epcList = new string[_epcList.Count];
+                _epcList.CopyTo(epcList, 0);
+
+                foreach (string epc in epcList)
+                {
+                    WbsFormattedEpcList += "|" + epc;
+                }
+
+                return WbsFormattedEpcList;
+            }
+            else
+                return null;
+        }
+    }
+}
