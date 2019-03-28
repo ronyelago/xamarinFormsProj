a6688882a6c357fd17c0a85df98b54331d705e14
Rodolfo Cortese <rodolfo.cortese@easyepi.com.br>
2019-03-28 15:42:12 -0300

    Usuário não consegue prosseguir sem permissão de localização
    Caso o mesmo negue a permissão, é informado de que não há como prosseguir.
    Caso o usuário marque "Não perguntar novamente", terá que alterar as permissões pelo menu do sistema mesmo

--- AppEpi/AppEpi/Views/Fiscalizacao.xaml.cs
+++ AppEpi/AppEpi/Views/Fiscalizacao.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using AppEpi.Models;
 using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace AppEpi.Views
@@ -10,33 +14,68 @@ namespace AppEpi.Views
         public Fiscalizacao()
         {
             InitializeComponent();
+
+            Task.Run(() =>
+                RequestPermission(Permission.Location)
+                );
+        }
+
+
+        // Solicita a permissão desejada e retorna o estado da mesma após interação do usuário
+        private async Task<PermissionStatus> RequestPermission(Permission permission)
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permission);
+                if (status != PermissionStatus.Granted)
+                {
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(permission);
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(permission))
+                        status = results[permission];
+                }
+                return status;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("RequestPermission Exception:" + e.Message);
+                return PermissionStatus.Unknown;
+            }
         }
 
 
         private async void btnConfirmar_Clicked(object sender, EventArgs e)
         {
-            var wbs = DependencyService.Get<IWEBClient>();
-
-            var location = await CrossGeolocator.Current.GetPositionAsync(TimeSpan.FromSeconds(10));
-            UsuarioLogado.Latitude = location.Latitude.ToString();
-            UsuarioLogado.Longitude = location.Longitude.ToString();
-
-            if (epcList.Count > 0)
+            try
             {
-                var answer = await DisplayAlert("Fiscalização", "Confirmar Fiscalização?\nTotal de Itens:" + epcList.Count, "Sim", "Não");
-                if (answer)
-                {
-                    var result = wbs.retornarDadosEpiValidar(epcList.GetFormattedEpcList(), UsuarioLogado.Cnpj, UsuarioLogado.FkCliente);
-                    UsuarioLogado.Operacao = "6";
-                    var detailPage = new Page4(result);
+                var wbs = DependencyService.Get<IWEBClient>();
 
-                    NavigationPage.SetBackButtonTitle(this, "Voltar");
-                    await Navigation.PushAsync(detailPage);
+                var location = await CrossGeolocator.Current.GetPositionAsync(TimeSpan.FromSeconds(10));
+                UsuarioLogado.Latitude = location.Latitude.ToString();
+                UsuarioLogado.Longitude = location.Longitude.ToString();
+
+                if (epcList.Count > 0)
+                {
+                    var answer = await DisplayAlert("Fiscalização", "Confirmar Fiscalização?\nTotal de Itens:" + epcList.Count, "Sim", "Não");
+                    if (answer)
+                    {
+                        var result = wbs.retornarDadosEpiValidar(epcList.GetFormattedEpcList(), UsuarioLogado.Cnpj, UsuarioLogado.FkCliente);
+                        UsuarioLogado.Operacao = "6";
+                        var detailPage = new Page4(result);
+
+                        NavigationPage.SetBackButtonTitle(this, "Voltar");
+                        await Navigation.PushAsync(detailPage);
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Fiscalização", "Verifique os Campos!", "OK");
                 }
             }
-            else
+            catch (Plugin.Geolocator.Abstractions.GeolocationException ex)
             {
-                await DisplayAlert("Fiscalização", "Verifique os Campos!", "OK");
+                Debug.WriteLine("Unauthorized Exception:" + ex.Message);
+                await DisplayAlert("Erro: Sem permissão", "Não é possível prosseguir sem permissão de acesso a localização!", "OK");
             }
         }
 
