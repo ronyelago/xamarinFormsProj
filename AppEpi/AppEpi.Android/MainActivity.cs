db05c9e38db35104b55b5135fcb1a0f489dbdd93
Rodolfo Cortese <rodolfo.cortese@easyepi.com.br>
2019-03-29 12:41:23 -0300

    Fix: Popups throwing exceptions
    Após atualização do Plugin houve necessidade de inicialização do Plugin na MainActivity
    vide documentação:
    https://github.com/rotorgames/Rg.Plugins.Popup/wiki/Migration-from-v1.0.x-to-v1.1.x

--- AppEpi/AppEpi.Android/MainActivity.cs
+++ AppEpi/AppEpi.Android/MainActivity.cs
@@ -7,6 +7,7 @@ using Plugin.Permissions;
 using Plugin.CurrentActivity;
 using Android.Runtime;
 using Xamarin.Forms;
+using Rg.Plugins.Popup;
 
 namespace AppEpi.Droid
 {
@@ -18,23 +19,38 @@ namespace AppEpi.Droid
         {
             base.OnCreate(bundle);
 
+            // Inicialização obrigatória de plugins
+            Popup.Init(this, bundle);
             CrossCurrentActivity.Current.Init(this, bundle);
             Forms.Init(this, bundle);
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-
             LoadApplication(new App());
             Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.TurnScreenOn);
         }
 
 
-        // Inicialização do Plugin Plugin.Permissions (vide documentação no GitHub)
+        // Parte da inicialização do Plugin Plugin.Permissions (vide documentação no GitHub)
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        // Parte da inicialização do Plugin Rg.Plugins.Popup
+        public override void OnBackPressed()
+        {
+            if (Popup.SendBackPressed(base.OnBackPressed))
+            {
+                // Do something if there are some pages in the `PopupStack`
+            }
+            else
+            {
+                // Do something if there are not any pages in the `PopupStack`
+            }
         }
     }
 }
